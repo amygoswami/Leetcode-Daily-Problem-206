94b47e5fe0707a2bf5f9154e7ceb122cb77b0e9c
amygoswami <77150010+amygoswami@users.noreply.github.com>
2024-03-24 12:33:36 +0530

    Time: 5 ms (50.00%) | Memory: 7.3 MB (16.67%) - LeetSync

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int f(int sum, int maxi, int k, vector<int>& dp){
-        //base case
-        if(sum >= k)    return 0;
-        if(dp[maxi] != -1)  return dp[maxi];
-        int notTake = 1 + f(sum + 1, maxi+1, k, dp);
-        int take = 1 + f(sum + maxi, maxi, k, dp);
-        return dp[maxi] = min(notTake, take);
-    }
-
     int minOperations(int k) {
-        vector<int> dp(k+1, -1);
-        return f(1, 1, k, dp);
+        int ans = INT_MAX;
+        for(int i=1;i<=k;i++){
+            int temp = ceil(k*1.0/i);
+            ans = min(ans,i-1+temp-1);
+        }
+        return ans;
     }
 };
