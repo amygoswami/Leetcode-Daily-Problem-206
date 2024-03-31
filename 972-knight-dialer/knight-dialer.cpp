29bbbdd22f05ba618f011593ad9c46e24a53ed96
amygoswami <77150010+amygoswami@users.noreply.github.com>
2024-04-01 00:03:26 +0530

    Time: 83 ms (56.78%) | Memory: 38.7 MB (37.75%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -4,27 +4,26 @@ public:   int mod = 1e9 + 7;
         {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}
     };
 
-    int dfs(int remain, int i, vector<vector<int>> &dp){
-        if(remain == 0)
-            return 1;
-        
-        if(dp[remain][i] != 0)  
-            return dp[remain][i];
-        
-        int ans = 0;
-        //explore neighbors
-        for(int j : jumps[i])
-            ans = (ans + dfs(remain-1, j, dp)) % mod;
-        
-        dp[remain][i] = ans;
-        return ans;
-    }
-
     int knightDialer(int n) {
         vector<vector<int>> dp(n+1, vector<int> (10, 0));
+        //base case
+        for(int i=0; i<10; i++)
+            dp[0][i] = 1; 
+
+        //write changing params ie remain and i
+        for(int remain=1; remain<n; remain++){
+            for(int i=0; i<10; i++){
+                int ans = 0;
+                //explore neighbors
+                for(int j : jumps[i])
+                    ans = (ans + dp[remain-1][j]) % mod;
+                dp[remain][i] = ans;
+            }
+        }
+
         int ans = 0;
         for(int i=0; i<10; i++)
-            ans = (ans + dfs(n-1, i, dp)) % mod;
+            ans = (ans + dp[n-1][i]) % mod;
         return ans;
     }
 };
