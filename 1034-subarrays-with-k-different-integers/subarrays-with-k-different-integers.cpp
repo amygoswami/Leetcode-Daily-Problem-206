08bb0c0be7e652269a0a3802481a3378a8eb32c3
amygoswami <77150010+amygoswami@users.noreply.github.com>
2024-03-31 12:47:47 +0530

    Time: 35 ms (95.42%) | Memory: 36.5 MB (97.67%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k){
-        //remove subarrays storing distinct elements < k & store only those w distinct elements = k 
-        return slidingWindowAtmost(nums, k) - slidingWindowAtmost(nums, k-1);   
-    }
-
-    int slidingWindowAtmost(vector<int>& nums, int K) {
-        unordered_map<int, int> mp;
-        int cnt = 0;
-        int i = 0, j = 0;
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        vector<int> distinctCnt(nums.size()+1, 0);
+        int totalCnt = 0;
+        int i = 0, j = 0, currCnt = 0;
 
         while(j < nums.size()){
-            mp[nums[j]]++;
-
-            while(mp.size() > K){
-                mp[nums[i]]--;
-                if(mp[nums[i]] == 0)
-                    mp.erase(nums[i]);
-                i++;
+            if(++distinctCnt[nums[j++]] == 1){
+                k--;    //decrement k when a new element is encountered
             }
-            
-            cnt += j-i+1;   //represents no of subarrays betn i and j
-            j++;
+            if(k < 0){
+                --distinctCnt[nums[i++]];
+                k++;
+                currCnt = 0;
+            }
+            //if k becomes 0, cnt no of subarrays
+            if(k == 0){
+                while(distinctCnt[nums[i]] > 1){
+                    --distinctCnt[nums[i++]];
+                    currCnt++;
+                }
+                totalCnt += currCnt + 1;
+            }
         }
-        return cnt;
+        return totalCnt;
     }
 };
