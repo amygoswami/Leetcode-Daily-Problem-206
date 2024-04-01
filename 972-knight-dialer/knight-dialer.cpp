88a2080443dd1429d931c7517108723d30354aa2
amygoswami <77150010+amygoswami@users.noreply.github.com>
2024-04-01 12:24:13 +0530

    Time: 5 ms (97.19%) | Memory: 7.2 MB (96.18%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,27 +1,18 @@
 class Solution {
-public:   int mod = 1e9 + 7;  
-    vector<vector<int>> jumps = {
-        {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}
-    };
-
+public:     int mod = 1e9 + 7;
     int knightDialer(int n) {
-        vector<int> prev(10, 1), cur(10, 0);
-
-        //write changing params ie remain and i
-        for(int remain=1; remain<n; remain++){
-            for(int i=0; i<10; i++){
-                int ans = 0;
-                //explore neighbors
-                for(int j : jumps[i])
-                    ans = (ans + prev[j]) % mod;
-                cur[i] = ans;
-            }
-            prev = cur;
+        if(n == 1)  return 10;  //10 ways to dial nos of length 1 each
+        int A = 4, B = 2, C = 2, D = 1; //total no of states
+        for(int i=0; i<n-1; i++){
+            int tmpA = A, tmpB = B, tmpC = C, tmpD = D;
+            A = ((2 * tmpB) % mod + (2 * tmpC) % mod) % mod;
+            B = tmpA;
+            C = (tmpA + (2 * tmpD) % mod) % mod;
+            D = tmpC;
         }
 
-        int ans = 0;
-        for(int i=0; i<10; i++)
-            ans = (ans + prev[i]) % mod;
-        return ans;
+        int ans = (A + B) % mod;
+        ans = (ans + C) % mod;
+        return (ans + D) % mod;
     }
 };
